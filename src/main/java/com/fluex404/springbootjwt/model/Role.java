2158045d6402a6b160146526fe411dfd70508c16
fluex404 <isaabqari@gmail.com>
2020-11-25 11:30:56 +0700

    nambahin model

--- src/main/java/com/fluex404/springbootjwt/model/Role.java
+++ src/main/java/com/fluex404/springbootjwt/model/Role.java
@@ -1,4 +1,23 @@
 package com.fluex404.springbootjwt.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
 }
