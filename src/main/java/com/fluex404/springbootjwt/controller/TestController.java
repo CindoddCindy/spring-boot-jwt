83bbe321ccf09f5a3b7cfb02146ce0f3eb6b5c55
fluex404 <isaabqari@gmail.com>
2020-11-25 15:12:50 +0700

    push aja dulu

--- src/main/java/com/fluex404/springbootjwt/controller/TestController.java
+++ src/main/java/com/fluex404/springbootjwt/controller/TestController.java
@@ -1,13 +1,26 @@
 package com.fluex404.springbootjwt.controller;
 
+import com.fluex404.springbootjwt.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Security;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+    @Value("${bezkoder.app.jwtSecret}")
+    private String jwtSecret;
+
     @GetMapping("/all")
     public String allAccess(){
         return "Public Content";
@@ -15,6 +28,10 @@ public class TestController {
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public String userAccess(){
+
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("user: "+user.getUsername()+" -> /api/test/user");
+
         return "User Content";
     }
     @GetMapping("/mod")
